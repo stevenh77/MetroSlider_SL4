d5bb87f64e91a9f9f00b3559cae9df2a31725684
Steven Hollidge <stevenhollidge@hotmail.com>
2012-07-17 13:25:07 +0200

    Initial upload

---
+++ MetroSlider_SL4/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿
+namespace MetroSlider_SL4
+{
+    public partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Option1_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            this.MySlider.Value = 1;
+        }
+
+        private void Option2_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            this.MySlider.Value = 2;
+        }
+
+        private void Option3_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            this.MySlider.Value = 3;
+        }
+    }
+}
